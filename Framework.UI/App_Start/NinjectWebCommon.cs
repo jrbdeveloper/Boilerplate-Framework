90238fe28d0f95ef6be1793bf73ff5de17b263aa
jbales <jrbdeveloper@gmail.com>
2016-12-06 20:59:04 -0800

    updated the people js module to be more robust

--- Framework.UI/App_Start/NinjectWebCommon.cs
+++ Framework.UI/App_Start/NinjectWebCommon.cs
@@ -61,8 +61,7 @@ namespace Framework.UI.App_Start
         /// <param name="kernel">The kernel.</param>
         private static void RegisterServices(IKernel kernel)
         {
-            kernel.Load(new DomainModule());
-            kernel.Load(new DataModule());
+            kernel.Load(new DomainModule(), new DataModule());
         }        
     }
 }
--- Framework.UI/Scripts/Modules/people.module.js
+++ Framework.UI/Scripts/Modules/people.module.js
@@ -1,12 +1,41 @@
-﻿$.widget("module.People", {
-    options: {},
-
-    _init: function () { },
-
-    _create: function () {
+﻿$.widget("custom.People", {
+    options: {
+        gridSettings: {
+            columns: [],
+            text_filters: [],
+            list_filters: []
+        }
     },
 
-    _registerEvents: function () { },
+    _init: function () {
+        var self = this;
 
-    _initializeControls: function () { },
+        self.Grid = $("#grid").empty().Grid({
+            pageSize: -1,
+            showFilters: true,
+            allowSelect: true,
+            fixedHeader: true,
+            ajax: {
+                url: "",
+                dataSrc: ""
+            },
+            textFilters: self.options.gridSettings.text_filters,
+            listFilters: self.options.gridSettings.list_filters,
+            columns: self.options.gridSettings.columns
+        });
+    },
+
+    _create: function () {
+        this.Application = Singleton.getInstance();
+        this._initializeControls();
+        this._registerEvents();
+    },
+
+    _registerEvents: function () {
+        var self = this;
+    },
+
+    _initializeControls: function () {
+
+    },
 });
