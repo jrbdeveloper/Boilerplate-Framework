366bfd2fca5d39c26724f101d9cdcdfef39ba8a0
jbales <jrbdeveloper@gmail.com>
2016-12-11 21:50:19 -0800

    Removing unwanted assets

--- Framework.UI/Controllers/HomeController.cs
+++ Framework.UI/Controllers/HomeController.cs
@@ -9,13 +9,6 @@ namespace Framework.UI.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
